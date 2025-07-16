76c067b8e81121c70ac35ec1a58089c944b351eb
Livia Kobayashi Machado Martins <liviakmmartins@gmail.com>
2025-07-15 22:11:14 -0300

    feat: creation of the input task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import "./styles/theme.css";
 import "./styles/global.css";
 import { Heading } from "./components/heading/heading";
+import TaskForm from "./components/task-form/task-form";
 export function App() {
   return (
-    <div className=" flex flex-col justify-center">
-      <Heading />
-      <h1 className="text-9xl font-bold text-center">00:00</h1>
-    </div>
+    <section className="flex flex-col justify-center items-center py-10 ">
+      <div className="container flex flex-col justify-center items-center gap-20">
+        <Heading />
+        <h1 className="text-9xl font-bold text-center ">00:00</h1>
+        <TaskForm />
+      </div>
+    </section>
   );
 }
---
+++ src/components/task-form/task-form.tsx
@@ -0,0 +1,8 @@
+export default function TaskForm() {
+  return (
+    <form className="flex flex-col justify-center items-center gap-2">
+      <label htmlFor="taskId">Task:</label>
+      <input type="text" id="taskId" className="border-b-1 border-[#0ba170]" />
+    </form>
+  );
+}
