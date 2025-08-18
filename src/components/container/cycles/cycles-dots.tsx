15c2dc8236b09af99bbacbcbf486476ee0772164
Livia Kobayashi <liviakmmartins@gmail.com>
2025-08-18 18:04:17 -0300

    feat: creation of cycles in hard code

---
+++ src/components/container/cycles/cycles-dots.tsx
@@ -0,0 +1,15 @@
+export default function CyclesDots() {
+    return(
+        <div className="flex gap-2">
+            <div className="w-6 h-6 bg-yellow-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-green-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-yellow-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-green-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-yellow-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-green-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-yellow-500 rounded-full"></div>
+            <div className="w-6 h-6 bg-blue-500 rounded-full"></div>
+
+        </div>
+    )
+}
---
+++ src/components/container/cycles/view/cycles-view.tsx
@@ -0,0 +1,10 @@
+import CyclesDots from "../cycles-dots";
+
+export default function CyclesView() {
+    return(
+        <>
+        <h2>Ciclos:</h2>
+        <CyclesDots/>
+        </>
+    )
+}
