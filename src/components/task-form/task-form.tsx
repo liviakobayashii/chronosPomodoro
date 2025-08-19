c3de76db059dff89b8c9d2093846f74281fdad41
Livia Kobayashi <liviakmmartins@gmail.com>
2025-08-19 15:29:09 -0300

    style: change in input styling

--- src/components/task-form/task-form.tsx
+++ src/components/task-form/task-form.tsx
@@ -2,7 +2,7 @@ export default function TaskForm() {
   return (
     <form className="flex flex-col justify-center items-center gap-2">
       <label htmlFor="taskId">Task:</label>
-      <input type="text" id="taskId" className="border-b-1 border-[#0ba170]" />
+      <input type="text" id="taskId" className="border-b-2 border-[#0ba170]" />
       <p className="mt-5">Nesse ciclo foque por 25min</p>
     </form>
   );
